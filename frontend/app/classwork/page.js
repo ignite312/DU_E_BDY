eb98eb4215c542ff0c559b93db162ea2341bc945
ignite312 <contact.emonkhan@gmail.com>
2024-02-17 16:03:51 +0600

    added some frontend

--- frontend/app/classwork/page.js
+++ frontend/app/classwork/page.js
@@ -1,32 +1,28 @@
-'use client'
-import React, { useState } from "react";
-import 'katex/dist/katex.min.css';
+import React from "react";
 import Link from 'next/link';
-
 
 export default function Classwork() {
     return (
-        <div className="bg-white text-black flex justify-center items-center h-screen">
-            <div className="flex flex-col md:flex-row items-center w-full md:w-4/5">
-            <ul className="menu menu-horizontal mb-4 md:mb-0 flex">
-    <li className="mr-2 flex-grow">
-        <Link href="/classwork/createnew">
-            <div className="btn btn-outline text-black w-full border border-black flex justify-center items-center">
-                Create New Question
+        <div className="bg-white text-black flex flex-col h-screen">
+            <div className="flex justify-end p-4">
+                <div>
+                    <Link href="/classwork/createnew">
+                        <div className="btn btn-outline text-black">
+                            Create New Question
+                        </div>
+                    </Link>
+                </div>
+                <div className="ml-2">
+                    <Link href="/classwork/upload">
+                        <div className="btn btn-outline text-black">
+                            Upload Question
+                        </div>
+                    </Link>
+                </div>
             </div>
-        </Link>
-    </li>
-    <li className="flex-grow">
-        <Link href="/classwork/upload">
-            <div className="btn btn-outline text-black w-full border border-black flex justify-center items-center">
-                Upload Question
-            </div>
-        </Link>
-    </li>
-</ul>
 
-
-                <div className="questions-container w-full md:w-3/4">
+            <div className="flex justify-center  flex-1">
+                <div className="w-1/2">
                     <ul className="questions-list space-y-4">
                         <li>
                             <div className="bg-gray-100 p-4 rounded-md shadow-md">
